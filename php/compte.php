f93a0fea5b8dc7a3220fad8744a1fb3b4d2717f0
François Poguet <francoispoguet@gmail.com>
2020-04-21 15:02:21 +0200

    compte.php : 3 sections ok + refactorisation

--- php/compte.php
+++ php/compte.php
@@ -3,6 +3,12 @@ ob_start();
 session_start();
 require_once('bibli_gazette.php');
 
+define('EDIT_PERSONAL_DATA',1);
+define('EDIT_PASS',2);
+define('EDIT_EDITOR_DATA',3);
+define('EDIT_PICTURE',4);
+
+
 // --- local functions ---
 
 /**
@@ -14,14 +20,17 @@ function cpl_fetch_userData(){
     $db = cp_db_connecter();
 
     $query = '('.'SELECT utPseudo AS pseudo, utNom AS last_name, utPrenom AS first_name, utEmail AS email, utDateNaissance AS birthday, utCivilite AS civility, utMailsPourris AS spam, reBio AS bio, reCategorie AS category, reFonction AS function, 1 AS type 
-                FROM utilisateur, redacteur
-                WHERE utPseudo = rePseudo
-                AND utPseudo="'. cp_db_protect_inputs($db,$_SESSION['pseudo']).'" ) UNION (SELECT 0,0,0, utEmail,0,0,0,0,0,0, 2 AS type FROM utilisateur WHERE utPseudo <> "'. cp_db_protect_inputs($db,$_SESSION['pseudo']).'" )';
+                FROM utilisateur LEFT OUTER JOIN redacteur ON utPseudo = rePseudo
+                WHERE utPseudo="'. cp_db_protect_inputs($db,$_SESSION['pseudo']).'" ) UNION (SELECT 0,0,0, utEmail,0,0,0,0,0,0, 2 AS type FROM utilisateur WHERE utPseudo <> "'. cp_db_protect_inputs($db,$_SESSION['pseudo']).'" ) ORDER BY type';
 
-    $userData = cp_db_execute($db,$query);
+    $userData = cp_db_execute($db,$query,false);
 
-    // We keep all others user's email for check later if the user want to change
-    $GLOBALS['emails'] = array_slice($userData,1);
+    if(isset($_POST['btnEditData'])){
+        // We keep all others user's email for check later if the user want to change
+        $GLOBALS['emails'] = array_slice($userData,1);
+    }else if(isset($_POST['btnEditBio']) && $userData[0]['bio'] == '' ){
+        $GLOBALS['emptyBio'] = true;
+    }
 
     $userData = $userData[0];
 
@@ -37,178 +46,214 @@ function cpl_fetch_userData(){
 
 /**
  * Check if it's a hacking case
+ * @param int $processType  The type of process
  * @return void|exit Exit the script if it's a hacking case
  */
-function cpl_hackGuardEditData(){
-     /*
-        Only the spam checkbox can be missing
-        So, if other keys are missing -> hacking.
-    */
-    $mandatoryKeys = ['btnEditData','civility','last_name','first_name','birthday_y','birthday_m','birthday_d','email'];
-    $optionalKeys = ['spam'];
+function cpl_hackGuard($processType){
+    switch($processType){
+        
+        case EDIT_PERSONAL_DATA : {
+            /* Only the spam checkbox can be missing. So, if other keys are missing -> hacking.*/
+            $mandatoryKeys = ['btnEditData','civility','last_name','first_name','birthday_y','birthday_m','birthday_d','email'];
+            $optionalKeys = ['spam'];
 
-    cp_check_param($_POST,$mandatoryKeys,$optionalKeys) or cp_session_exit('../index.php');
+            cp_check_param($_POST,$mandatoryKeys,$optionalKeys) or cp_session_exit('../index.php');
 
-    // If the date fields values are invalid -> hacking
-    cp_isValid_date($_POST['birthday_d'],$_POST['birthday_m'],$_POST['birthday_y']) or cp_session_exit('../index.php');
+            // If the date fields values are invalid -> hacking
+            cp_isValid_date($_POST['birthday_d'],$_POST['birthday_m'],$_POST['birthday_y']) or cp_session_exit('../index.php');
 
-     // If the value of civility is different of 'h' and 'f' -> hacking
-    cp_isValid_civility($_POST['civility']) or cp_session_exit('../index.php');
+            // If the value of civility is different of 'h' and 'f' -> hacking
+            cp_isValid_civility($_POST['civility']) or cp_session_exit('../index.php');
+            return;
+        }
 
+       
+        case EDIT_PASS : {
+            $mandatoryKeys = ['pass2','pass1','btnEditPass'];
+            cp_check_param($_POST,$mandatoryKeys) or cp_session_exit('../index.php');
+            return;
+        }
+
+        
+        case EDIT_EDITOR_DATA : {
+            $mandatoryKeys = ['btnEditBio','category','function','bio'];
+            cp_check_param($_POST,$mandatoryKeys) or cp_session_exit('../index.php');
+            cp_intIsBetween($_POST['category'],1,3) or cp_session_exit('../index.php');
+            return ;
+        }
+    }
 }
 
 /**
  * Check if the user made mistakes while editing his data
+ * @param int $processType  The type of process
  * @return Array The errors list
  */
-function cpl_checkEditingMistakes(){
+function cpl_checkMistakes($processType){
     $_POST = array_map('trim',$_POST);
     $errors = array();
 
-    // Last/First name
-    foreach(['last_name','first_name'] as $value){
-        $french = ($value == 'last_name') ? 'nom' : 'prénom';
+    switch($processType){
+        case EDIT_PERSONAL_DATA : {
+             // Last/First name
+            foreach(['last_name','first_name'] as $value){
+                $french = ($value == 'last_name') ? 'nom' : 'prénom';
 
-        $maxLength = ($value == 'last_name') ? 50 : 60;
-        $res = cp_isValid_name($_POST[$value],$maxLength);
+                $maxLength = ($value == 'last_name') ? 50 : 60;
+                $res = cp_isValid_name($_POST[$value],$maxLength);
+                
+                if($res == 1){
+                    $errors[] = "Le $french ne doit pas être vide";
+                    continue;
+                }
+                if($res == 2){
+                    $errors[] = "Le $french ne doit pas contenir de tags HTML";
+                    continue;
+                }
+                
+                if($res == 3){
+                    $errors[] = "Le $french doit contenir moins de $maxLength caractères";
+                    continue;
+                }
+            }
+
+            // Age
+            if(!cp_isValid_age($_POST['birthday_d'],$_POST['birthday_m'],$_POST['birthday_y'])){
+                $errors[] = 'Vous devez avoir plus de 18 ans pour vous inscrire';
+            }
+
+            // Email
+            $emailValid = cp_isValid_email($_POST['email']);
+            if($emailValid == 1){
+                $errors[] = 'L\'adresse email n\'est pas valide';
+            }else if($emailValid == 2){
+                $errors[] = 'L\'adresse mail doit contenir moins de 256 caractères';
+            }
+
+
+            foreach($GLOBALS['emails'] as $email){
+                if($email['email'] == $_POST['email'] ){
+                    $errors[] = 'L\'adresse mail est déjà utilisée';
+                    break;
+                }
+            }
+            unset($GLOBALS['emails']);
+        break;
+        }
+        case EDIT_PASS : {
+            // Passe1 et Passe2
+            $passValid = cp_isValid_pass($_POST['pass1'],$_POST['pass2']);
+            if($passValid == 1 ){
+                $errors[] = 'Le mot de passe ne doit pas être vide';
+            }else if($passValid == 2 ){
+                $errors[] = 'Les mots de passes doivent être identiques';
+            }else if($passValid == 3){
+                $errors[] = 'Le mot de passe doit contenir moins de 256 caractères';
+            }
+        break;
+        }
+        case EDIT_EDITOR_DATA : {
+            if($_POST['function'] != strip_tags($_POST['function'])){
+                $errors[] = 'La fonction ne doit pas contenir de tags html';
+            }
         
-        if($res == 1){
-            $errors[] = "Le $french ne doit pas être vide";
-            continue;
-        }
-        if($res == 2){
-            $errors[] = "Le $french ne doit pas contenir de tags HTML";
-            continue;
-        }
+            if(strlen($_POST['function']) > 100){
+                $errors[] = 'La fonction ne doit pas dépasser 100 caractères';
+            }
         
-        if($res == 3){
-            $errors[] = "Le $french doit contenir moins de $maxLength caractères";
-            continue;
+            if(strlen($_POST['bio']) == 0){
+                $errors[] = 'La biographie ne peut pas être vide';
+            }
+        
+            if($_POST['bio'] != strip_tags($_POST['bio'])){
+                $errors[] = 'La biographie ne doit pas contenir de tags HTML';
+            }
+        break;
         }
     }
-
-    // Age
-    if(!cp_isValid_age($_POST['birthday_d'],$_POST['birthday_m'],$_POST['birthday_y'])){
-        $errors[] = 'Vous devez avoir plus de 18 ans pour vous inscrire';
-    }
-
-    // Email
-    $emailValid = cp_isValid_email($_POST['email']);
-    if($emailValid == 1){
-        $errors[] = 'L\'adresse email n\'est pas valide';
-    }else if($emailValid == 2){
-        $errors[] = 'L\'adresse mail doit contenir moins de 256 caractères';
-    }
-
-    // var_dump($_SESSION['emails']);
-
-    foreach($GLOBALS['emails'] as $email){
-        if($email['email'] == $_POST['email'] ){
-            $errors[] = 'L\'adresse mail est déjà utilisée';
-            break;
-        }
-    }
-
     return (count($errors) == 0)?0:$errors;
 
 }
 
 /**
- * Update the user data in database
+ * Update data on database
+ * @param int $processType  The type of process
  */
-function cpl_updateData(){
+function cpl_updateDatabase($processType){
     $db = cp_db_connecter();
+    $protected = array_slice($_POST,0,null,true); // clone $_POST to $protected (important because otherwise, the value displayed after the update will be escaped on the html form) -> *1
+    $protected = cp_db_protect_inputs($db,$protected);
+    $pseudo = cp_db_protect_inputs($db,$_SESSION['pseudo']);
 
-    $_POST = cp_db_protect_inputs($db,$_POST);
-    extract($_POST);
-    $birthDate = $birthday_y*10000+$birthday_m*100+$birthday_d;
-    $spam = (isset($spam)) ? 1:0;
+    switch($processType){
+       
+        case EDIT_PERSONAL_DATA : {
+            extract($protected);
+            $birthDate = $birthday_y*10000+$birthday_m*100+$birthday_d;
+            $spam = (isset($spam)) ? 1:0;
 
-    $query = "UPDATE utilisateur
-                SET utCivilite = '$civility',
-                    utNom = '$last_name',
-                    utPrenom = '$first_name',
-                    utEmail = '$email',
-                    utDateNaissance = '$birthDate',
-                    utMailsPourris = '$spam'
-                WHERE  utPseudo = '". cp_db_protect_inputs($db,$_SESSION['pseudo'])."'";
+            $query = "UPDATE utilisateur
+                        SET utCivilite = '$civility',
+                            utNom = '$last_name',
+                            utPrenom = '$first_name',
+                            utEmail = '$email',
+                            utDateNaissance = '$birthDate',
+                            utMailsPourris = '$spam'
+                        WHERE  utPseudo = '$pseudo'";
+        break;
+        }
+        
+        case EDIT_PASS : {
+            $pass = $protected['pass1'];
+            $pass =  password_hash($pass,PASSWORD_DEFAULT);
+            
+            $query = "UPDATE utilisateur
+                        SET utPasse = '$pass'
+                        WHERE  utPseudo = '$pseudo'";
 
+        break;
+        }
+        
+        case EDIT_EDITOR_DATA : {
+            $bio = $protected['bio'];
+            $function =$protected['function'];
+            $category = $protected['category'];
+
+            if(isset($GLOBALS['emptyBio'])){
+                $query = "INSERT INTO redacteur SET rePseudo = '$pseudo', reBio = '$bio', reFonction = '$function', reCategorie = '$category'"; 
+            }else{
+                $query = "UPDATE redacteur SET reBio = '$bio', reFonction = '$function', reCategorie = '$category' WHERE rePseudo = '$pseudo' ";
+            }
+
+        break;
+        }
+    }
     cp_db_execute($db,$query,false,true);
-
     mysqli_close($db);
 }
+
 
 /**
  * Edit the user's data 
+ * @param int $processType  The type of process
  * @return Array|0 The array of errors, 0 if there are no errors
  */
-function cpl_editDataProcess(){
+function cpl_editDataProcess($processType){
     // Avoid hacking case
-    cpl_hackGuardEditData();
+    cpl_hackGuard($processType);
 
     // check user mistakes
-    if(($errors = cpl_checkEditingMistakes()) != 0){
+    if(($errors = cpl_checkMistakes($processType))){
         return $errors;
     }
 
-    cpl_updateData();
+    // Update data on database
+    cpl_updateDatabase($processType);
 
     return 0;
-    
 }
 
 
-// EDIT PASS
-
-
-/**
- * Update the user's pass on database
- */
-function cpl_updatePass(){
-    $db = cp_db_connecter();
-    $pass = $_POST['pass1'];
-    $pass =  password_hash($pass,PASSWORD_DEFAULT);
-
-    $query = "UPDATE utilisateur
-                SET utPasse = '$pass'
-                WHERE  utPseudo = '". cp_db_protect_inputs($db,$_SESSION['pseudo'])."'";
-
-    cp_db_execute($db,$query,false,true);
-
-    mysqli_close($db);
-
-}
-
-/**
- * Edit the user's password
- * @return Array|0 The array of errors, 0 if there are no errors
- */
-function cpl_editPassProcess(){
-    $mandatoryKeys = ['pass2','pass1','btnEditPass'];
-    cp_check_param($_POST,$mandatoryKeys) or cp_session_exit('../index.php');
-
-    $errors = array();
-    $_POST = array_map('trim',$_POST);
-
-    // Passe1 et Passe2
-    $passValid = cp_isValid_pass($_POST['pass1'],$_POST['pass2']);
-    if($passValid == 1 ){
-        $errors[] = 'Le mot de passe ne doit pas être vide';
-    }else if($passValid == 2 ){
-        $errors[] = 'Les mots de passes doivent être identiques';
-    }else if($passValid == 3){
-        $errors[] = 'Le mot de passe doit contenir moins de 256 caractères';
-    }
-
-    if($errors){
-        return $errors;
-    }
-    
-    cpl_updatePass();
-
-    return 0;
-
-}
 
 // PRINT
 
@@ -228,6 +273,7 @@ function cpl_print_Errors($errors){
 }
 
 
+
 /**
  * Print the personal user page
  * @param Array $userData The connected user's data
@@ -240,11 +286,12 @@ function cpl_print_page_compte($userData = [], $errors = []){
     $birthday_m = substr($birthday,-4,2);
     $birthday_y = substr($birthday,0,4);
 
-    // if(isset($_POST['btnEditData'])){
-        $_POST = cp_db_protect_outputs($_POST);
-        extract($_POST,EXTR_OVERWRITE);
-        $spam = isset($spam);
-    // }
+    
+    $_POST = cp_db_protect_outputs($_POST); // *1
+    extract($_POST,EXTR_OVERWRITE);
+    if(isset($_POST['btnEditData'])){
+        $spam = isset($_POST['spam']);
+    }
     
 
     cp_print_beginPage('compte',"Mon compte",1,$_SESSION['status'],$_SESSION['pseudo']);
@@ -252,8 +299,8 @@ function cpl_print_page_compte($userData = [], $errors = []){
     echo '<section>',
             '<h2>Informations personnelles</h2>',
             '<p>Vous pouvez modifier les informations suivantes :</p>',
-            (count($errors) != 0 && isset($_POST['btnEditData'])) ? cpl_print_Errors($errors) : '',
-            (count($errors) == 0 && isset($_POST['btnEditData'])) ? '<p class="success">Vos informations ont été mise à jour avec succès.</p>':'',
+            ($errors && isset($_POST['btnEditData'])) ? cpl_print_Errors($errors) : '',
+            (!$errors && isset($_POST['btnEditData'])) ? '<p class="success">Vos informations ont été mise à jour avec succès.</p>':'',
             '<form method="POST" action="compte.php">',
                 '<table class="form">',
                     cp_form_print_radiosLine('Votre civilité :','civility',['Monsieur' => 'h','Madame'=> 'f'],$required,$civility,'',true),
@@ -269,8 +316,8 @@ function cpl_print_page_compte($userData = [], $errors = []){
             '<section>',
                 '<h2>Authentification</h2>',
                 '<p>Vous pouvez modifier votre mot de passe ci-dessous :</p>',
-                (count($errors) != 0 && isset($_POST['btnEditPass'])) ? cpl_print_Errors($errors) : '',
-                (count($errors) == 0 && isset($_POST['btnEditPass'])) ? '<p class="success">Votre mot de passe à été mis à jour avec succès.</p>':'',
+                ($errors && isset($_POST['btnEditPass'])) ? cpl_print_Errors($errors) : '',
+                (!$errors && isset($_POST['btnEditPass'])) ? '<p class="success">Votre mot de passe à été mis à jour avec succès.</p>':'',
                 '<form method="POST" action="compte.php">',
                     '<table class="form">',
                         cp_form_print_inputLine('Choisissez un mot de passe :','password','pass1',255,$required),
@@ -288,12 +335,14 @@ function cpl_print_page_compte($userData = [], $errors = []){
     echo '<section>',
             '<h2>Information rédacteur</h2>',
             '<p>Vous pouvez modifier les informations suivantes :</p>',
+            ($errors && isset($_POST['btnEditBio'])) ? cpl_print_Errors($errors) : '',
+            (!$errors && isset($_POST['btnEditBio'])) ? '<p class="success">Vos informations de rédacteur ont été mis à jour avec succès.</p>':'',
             '<form method="POST" action="compte.php">',
                 '<table class="form" >',
-                    cp_form_print_inputLine('Votre fonction :','text','function',100,true,'',$function),
+                    cp_form_print_inputLine('Votre fonction :','text','function',100,false,'',$function),
                     cp_form_print_listLine('Votre catégorie :','category',['Rédacteur en chef' => '1','Premier violon' => '2',  'Sous-fifre' => '3'],$category),
                     cp_form_print_textAreaLine('Votre biographie :','bio',str_replace(array("\n","\r"),'',$bio),60,4),
-                    cp_form_print_buttonsLine(['Enregistrer','editBioBtn'],'Réinitialiser'),
+                    cp_form_print_buttonsLine(['Enregistrer','btnEditBio'],'Réinitialiser'),
                 '</table>',
             '</form>',
 
@@ -305,7 +354,7 @@ function cpl_print_page_compte($userData = [], $errors = []){
             '<form method="POST" action="compte.php">',
                 '<table class="form" >',
                     cp_form_print_file('picture','',true,'Pour ne pas être déformée, la photo doit faire 150x200 pixels.',true),
-                    cp_form_print_buttonsLine(['Enregistrer','editPictureBtn'],'',true),
+                    cp_form_print_buttonsLine(['Enregistrer','btnEditPicture'],'',true),
                 '</table>',
             '</form>',
 
@@ -317,32 +366,34 @@ function cpl_print_page_compte($userData = [], $errors = []){
 }
 
 
-
-
-
-
 // --- Page generation
 
 
 cp_is_logged('../index.php');
 
+$userData = cpl_fetch_userData();
+
 if(isset($_POST['btnEditData'])){ 
-    $userData = cpl_fetch_userData(); 
-    $errors = cpl_editDataProcess();
-    cpl_print_page_compte($userData,($errors == 0)?[]:$errors);  
+    $errors = cpl_editDataProcess(EDIT_PERSONAL_DATA);
+    cpl_print_page_compte($userData,($errors)?$errors:[]);  
 
 }else if(isset($_POST['btnEditPass'])){
-    $userData = cpl_fetch_userData();
-    $errors = cpl_editPassProcess();
-    cpl_print_page_compte($userData,($errors == 0)?[]:$errors);
+    $errors = cpl_editDataProcess(EDIT_PASS);
+    cpl_print_page_compte($userData,($errors)?$errors:[]);
+
+}else if(isset($_POST['btnEditBio'])){
+    $errors = cpl_editDataProcess(EDIT_EDITOR_DATA);
+    cpl_print_page_compte($userData,($errors)?$errors:[]);
+
+}else if(isset($_POST['btnEditPicture'])){
+    
 
 }else{
-    $userData = cpl_fetch_userData();
     cpl_print_page_compte($userData);   
 }
 
 
 /** TODO
- * Faire la modification de bio,fonction, categorie et photo dans la base de données
+ * Faire la modification de et photo dans la base de données
  * optimiser : si pas de mofif, pas de co à la db
  */
